7119aeac8ce66d4d96812bb0404f12318c0b3438
Jim South <james.a.south@gmail.com>
2018-06-22 13:42:43 -0400

    Initial Controller Input

---
+++ Assets/ScriptableObjects/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameManager", menuName = "GameManager", order = 1)]
+public class GameManager : ScriptableObject {
+
+    public enum GameState
+    {
+        Title,
+        RoundActive,
+        Victory,
+        Pause
+    }
+    
+    [SerializeField]
+    private GameState initialState;
+
+    [SerializeField]
+    private GameState currentState;
+
+    public GameState CurrentState { get { return currentState; } }
+
+    public void SetState(GameState nextState)
+    {
+        currentState = nextState;
+    }
+
+    public void OnEnable()
+    {
+        SetState(initialState);
+    }
+}
---
+++ Assets/Scripts/Trash/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public GameManager gameManager;
+
+	// Use this for initialization
+	void Awake () {
+        Debug.Log(gameManager.CurrentState);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
