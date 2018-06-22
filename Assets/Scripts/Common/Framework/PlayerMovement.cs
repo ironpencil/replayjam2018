51579e4404d7aca925b29b4cce427ef5cae0d1eb
Sean K Smith <ssmith2347@gmail.com>
2018-06-22 14:17:30 -0400

    Started basic player movement

---
+++ Assets/Scripts/Common/Framework/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Rewired;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    public int playerId;
+    [SerializeField]
+    private Player rwPlayer;
+
+    public float speed;
+
+    void Awake()
+    {
+        rwPlayer = ReInput.players.GetPlayer(playerId);
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float x = rwPlayer.GetAxis("Horizontal");
+        float y = rwPlayer.GetAxis("Vertical");
+
+        Debug.Log(String.Format("x: {0}, y: {1}", x, y));
+    }
+}
