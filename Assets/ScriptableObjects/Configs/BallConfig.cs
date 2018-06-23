a77f02a8add71c2ac7879ccdb2a41c7b5a814da7
Jim South <james.a.south@gmail.com>
2018-06-23 01:13:41 -0400

    Ball bouncing on screen

---
+++ Assets/ScriptableObjects/Configs/BallConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Configs/Ball")]
+public class BallConfig : ScriptableObject {
+
+    public float initialSpeed = 10;
+    public float mass = 1;
+    public float addSpeedOnHit = 1;
+    public float maxSpeed = 20;
+}
