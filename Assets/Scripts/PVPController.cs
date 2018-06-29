67b6711258094237354aed399afe922dc50d003b
Jim South <james.a.south@gmail.com>
2018-06-29 15:37:35 -0400

    OOF

---
+++ Assets/Scripts/PVPController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PVPController : MonoBehaviour {
+
+    public PlayerData playerData;
+
+    public AudioEvent playerHitSound;
+    public AudioSource audioSource;
+
+    public void Hit(int playerNum)
+    {
+        if (playerNum != playerData.playerId)
+        {
+            //do hit
+            playerHitSound.Play(audioSource);
+        }
+    }
+}
